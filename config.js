454aaf0d8396d31b339e32f03bb076cb01c3ce6e
eptaccio <eptaccio@gmail.com>
2020-09-10 19:56:45 -0300

    chore: update host

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
   botToken: process.env.BOT_TOKEN,
   appPort: process.env.APP_PORT || 3000,
   isDevelopment: process.env.NODE_ENV === 'dev',
-  currentHost: 'https://rockleebr.vercel.app'
+  currentHost: 'https://rock-lee.vercel.app'
 }
 
 module.exports = {
